8ca416ba4902c8c0ace3eab41b0017daba13cd53
Andreas Robecke <a.robecke@hellogetsafe.com>
2023-11-02 20:29:01 +0100

    Initial commit

---
+++ lib/nxt_try.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nxt_try/version"
+
+module NxtTry
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nxt_try/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NxtTry
+  VERSION = "0.1.0"
+end
